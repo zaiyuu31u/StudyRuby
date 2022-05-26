604ae7595d61cd8db04eb66522754df4a20304eb
zaiyuu31u <58914778+zaiyuu31u@users.noreply.github.com>
2022-05-26 23:29:32 +0900

    Update 5.rb

--- 5.rb
+++ 5.rb
@@ -41,5 +41,59 @@ currencies['japan'] = 'yen'
  
 # 取得
 currencies['japan']
+currencies['brazil'] #=> 存在しないキーを指定するとnilが返る
 
 # 5.2.2
+# 繰り返し処理
+# ブロックパラメータがキーと値で2個になっている
+currencies.each do |key, value|
+  puts "#{key}: #{value}"
+end
+
+# ブロックパラメータを一つにするとキーと値が配列に格納される
+currencies.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key}: #{value}"
+end
+
+# 5.2.3
+# ハッシュの同値比較、要素数の取得、要素の削除
+# == すべてのキーと値が同じならtrue
+a = { 'x' => '1', 'y' => '2', 'z' => '3' }
+b = { 'x' => '1', 'y' => '2', 'z' => '3' }
+a == b #=> true
+
+# 並び順が異なっていてもキーと値がすべて同じならtrue
+c = { 'z' => '3', 'x' => '1', 'y' => '2' }
+a == c #=> true
+
+# size lengthでハッシュの要素の数を取得できる
+{}.size #=> 0
+a.size
+a.length
+
+# delete(削除したい要素のキー)で要素を指定して削除できる
+# 戻り値は削除された要素の値
+currencies.delete('japan') #=> 'yen'
+# キーがなければnilが返る
+currencies.delete('korea') #=> nil
+# ブロックを渡すと、nilの代わりにブロックの戻り値を返せる
+currencies.delete('korea') { |key| "Not found: #{key}" } #=> 'Not found: korea'
+
+# 5.3
+# シンボルとは任意の文字列と一対一に対応するオブジェクト
+# 文字列の代わりに用いることもできるが、必ず白文字列と同じ振る舞いをするわけではない
+# 同じ内容のシンボルは必ず同一のオブジェクト
+:apple.object_id === :apple.object_id
+
+# シンボルはSymbolクラスのオブジェクト
+# シンボルはRuby内部で整数として管理されるので、文字列よりも高速に処理できる
+'apple' == 'apple'
+:apple == :apple
+
+# シンボルはイミュータブル
+:apple.upcase! #=> NoMethodError
+
+# 5.3.2
+ 
